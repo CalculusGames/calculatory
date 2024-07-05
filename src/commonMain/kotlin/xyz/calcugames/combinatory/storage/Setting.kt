d7ea4230a2933a57e6b44eade64fd1b284bd9f58
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-07-05 18:29:04 -0500

    Add Settings

--- src/commonMain/kotlin/xyz/calcugames/combinatory/storage/Setting.kt
+++ src/commonMain/kotlin/xyz/calcugames/combinatory/storage/Setting.kt
@@ -4,6 +4,7 @@ import korlibs.io.async.launch
 import korlibs.io.serialization.json.Json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import xyz.calcugames.combinatory.comma
 
 /**
  * Represents the settings for a game.
@@ -16,7 +17,7 @@ class Setting<T>(
     /**
      * The display name of the setting.
      */
-    val name: String = id.split("_").joinToString(" ") { word -> word.replaceFirstChar { it.uppercaseChar() } },
+    val name: String,
     /**
      * All possible values for the setting.
      */
@@ -39,13 +40,57 @@ class Setting<T>(
     val defaultValue: T = values.first()
 ) {
 
+    private val serializedDefaultValue = serializer(defaultValue)
+
+    /**
+     * Formats the value for display.
+     * @param value The value to format.
+     * @return The formatted value.
+     */
+    fun format(value: T): String
+        = "$name: ${presenter(value)}"
+
+    init {
+        require(values.isNotEmpty()) { "Values must not be empty" }
+    }
+
+    operator fun invoke(): T = get(this)
+
     companion object {
         // Settings
 
+        /**
+         * Whether to play music.
+         */
+        val MUSIC = boolean("music")
+
+        /**
+         * Whether to play sound effects.
+         */
+        val SFX = boolean("sfx")
+
+        /**
+         * Whether to show the scoreboard, which represents your current score.
+         */
         val SHOW_SCOREBOARD = boolean("show_scoreboard")
 
+        /**
+         * The GUI scale for the game.
+         */
+        val GUI_SCALE = numbers("gui_scale", "GUI Scale", range(0.5, 2.0, 0.25), 1.0)
+
+        /**
+         * All settings for the game.
+         */
+        val allSettings = setOf(
+            MUSIC,
+            SFX,
+            SHOW_SCOREBOARD,
+            GUI_SCALE
+        )
+
         // API
-        fun boolean(
+        private fun boolean(
             id: String,
             name: String = id.split("_").joinToString(" ") { word -> word.replaceFirstChar { it.uppercaseChar() } }
         ) = Setting(
@@ -57,7 +102,26 @@ class Setting<T>(
             { it as? Boolean }
         )
 
-        internal val settings: MutableMap<String, Any> = mutableMapOf()
+        private fun numbers(
+            id: String,
+            name: String = id.split("_").joinToString(" ") { word -> word.replaceFirstChar { it.uppercaseChar() } },
+            range: Iterable<Number>,
+            default: Number
+        ) = Setting(
+            id,
+            name,
+            range.map { it.toDouble() },
+            { it.comma },
+            { it },
+            { it as? Double },
+            default.toDouble()
+        )
+
+        // Storage
+
+        internal val settings: MutableMap<String, Any> = allSettings
+            .associate { setting -> setting.id to setting.serializedDefaultValue }
+            .toMutableMap()
 
         operator fun <T> get(setting: Setting<T>): T {
             return setting.deserializer(settings[setting.id]) ?: setting.defaultValue
